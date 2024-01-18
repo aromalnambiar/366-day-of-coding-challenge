98c243997ed7d0238dbd31346b9b1d990df55bff
aromalnambiar <aromalndr@gmail.com>
2024-01-18 08:00:52 +0530

    deleting element from the array using python

---
+++ Arrays/deletingElementIn2DimensionalArray.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Creating 2D array
+twoDimensionalArray = np.array([[1, 2, 3], [3, 4, 5], [6, 7, 8], [9, 10, 11]])
+
+
+# delete element from array using numpy delete method
+deletingElement = np.delete(twoDimensionalArray, 0, axis=1)
+
+print(deletingElement)
+
--- Arrays/searchingElementInTwoDimensionalArray.py
+++ Arrays/searchingElementInTwoDimensionalArray.py
@@ -1,16 +1,21 @@
 import numpy as np
 
-# creating an array
+# Creating an array
 newArray = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]) 
 
 
 # funtion for searching an element in the array
 def findArray(array, element):
-    for i in range(len(array)):
+    # TC - O(mn) , SC - O(1)
+    for i in range(len(array)): 
+        # TC - O(n) , SC - O(1)
         for j in range(len(array[0])):
+            # TC - O(1) , SC - O(1)
             if array[i][j] == element:
                 return "The position of the element " + str(element) + " is " + str(i) + " " + str(j)
     return "Element not found in the array"
 
 
 print(findArray(newArray, 10))
+
+# So TC - O(n^2) and SC - O(1)
