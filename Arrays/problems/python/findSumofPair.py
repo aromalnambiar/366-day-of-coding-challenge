2ddb90780cd79791437f42ddfdc1774b2d5048a7
aromalnambiar <aromalndr@gmail.com>
2024-01-26 12:06:39 +0530

    Another method

--- Arrays/problems/python/findSumofPair.py
+++ Arrays/problems/python/findSumofPair.py
@@ -12,3 +12,24 @@ def find_sum_pair(nums, target):
                 print([nums[i], nums[j]])
                 
 find_sum_pair(my_list, target)
+
+
+
+# another method using dictionary and enumarate funtion
+
+def find_pair_ans(nums, target):
+    # empty dictionary
+    data = {}
+    
+    # i represents the index and num represents the elements in array of nums. The enumarate funtion will iterate throught the all element in the list
+    for i, num in enumerate(nums):
+        # find the complement
+        complement = target - num
+        
+        if complement in data:
+            print(f"Indices of the two numbers are: {[data[complement], i]}")
+        
+        data[num] = i
+        
+        
+find_pair_ans(my_list, target)
