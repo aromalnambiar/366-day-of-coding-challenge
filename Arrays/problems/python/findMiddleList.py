80a1e214c9c13d88352a38c25c24c8ea4bb78fc1
aromalnambiar <aromalndr@gmail.com>
2024-01-27 10:19:32 +0530

    find the sum of diagonal matrix

---
+++ Arrays/problems/python/findMiddleList.py
@@ -0,0 +1,6 @@
+myList = [1, 2, 3, 4]
+
+def middle(lst):
+    return lst[1:-1]
+    
+print(middle(myList))
---
+++ Arrays/problems/python/findSumOfDiagonal.py
@@ -0,0 +1,17 @@
+
+my_list = [[1,2,3],[4,5,6],[7,8,9]] 
+
+def diagonal_sum(matrix):
+    place = 0
+    sum = 0
+    
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j == place:
+                sum = sum + matrix[i][place]
+        place = place + 1
+
+    return sum
+
+
+print(diagonal_sum(my_list))
