7ea3245847b4c253f74b4488ead68b2359e09b05
aromalnambiar <aromalndr@gmail.com>
2024-01-29 20:43:47 +0530

    check wheather it has same frequency another

--- List/problems/checkTheSamefrequencyOfList.py
+++ List/problems/checkTheSamefrequencyOfList.py
@@ -14,3 +14,15 @@ def check_same_frequency(list1, list2):
 
     
 print(check_same_frequency(list1, list2))
+
+
+# another method
+
+    def check_same_frequency(list1, list2):
+        def count_elements(lst):
+            counter = {}
+            for element in lst:
+                counter[element] = counter.get(element, 0) + 1
+            return counter
+        
+        return count_elements(list1) == count_elements(list2)
